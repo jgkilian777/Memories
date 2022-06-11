39fefe9abf185e4c13ed57c21dc11eb0c4b45762
Josh Kilian <j.kilian.15@aberdeen.ac.uk>
2022-06-12 08:59:03 +1000

    Base

---
+++ src/test/java/com/jgkilian777/memories/MemoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgkilian777.memories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
